822eea8859bbac9e2db1d29a924baa9a174a7c9e
Sophie <sophiewulfing@gmail.com>
2022-02-28 13:57:53 -0500

    02.28

--- NR913/Model_SmithPaper.R
+++ NR913/Model_SmithPaper.R
@@ -5,52 +5,51 @@ cat("
 
 # PRIORS
 
-alpha_hat ~ dnorm(0, 1000)
+for (j in 1:nsite){
+  alpha[j] ~ dnorm(mu, tau)
+}
+
+#These are our hyperparameters- aka a parameter of a prior distribution as opposed to paramter of a model
+  #means and st. devs across all sites
+  
+mu ~ dnorm(0, 0.001) #ahat from slide
+sigma ~ dunif(0,10)
+tau <- 1/sigma ^ 2
+
 b_rkm ~ dnorm(0, 1000)
 b_netDepth ~ dnorm(0, 1000)
 b_distDown ~ dnorm(0, 1000)
 b_distUp ~ dnorm(0, 1000)
 b_distDetect ~ dnorm(0, 1000)
-sigma ~ unif(0,10)
-tau <- 1/sigma ^ 2
 
 
 # LIKELIHOOD
 
-  for (j in 1:s){ #OK so it's a little weird how I did this. I have 3 for loops, one going throught each site (j terms) another going through each net series index (i terms) and then each data point withing that (k terms) I realize if we were using an actual dataset, this might iterate funny unless I were to set up data correctly, but otherwise would this be the general way to set up this model?
+  for (i in 1:ncatch){
+    eta[i] <- log(x$deployment_time[i]) #accounts for the unbalanced effort (h) among net series and is equal to the loge duration of time for which net series i was deployed
+    
+    x$c[i] ~ dpois(lambda[i])
+    
+    log(lambda[i]) <- eta[i] + alpha[x$site[i]] + b_rkm*x$rkm[i] + b_netDepth*x$netDepth[i] + b_distDown*x$distDown[i] + b_distUp*x$distUp[i] + b_distDetect*x$distDetect[i] #Log Link Function
+    
+} #end of model
   
-    alpha[j] ~ dnorm(alpha_hat, tau)
-    
-    for(i in 1:n){
-      eta[i] <- log(time_net_series_deployed)
-      
-      for(k in 1:d){
-    
-        c_ij[k] ~ dpois(exp(log_link)) #Fix log link fxn
-        log_link <- eta[i] + alpha[j] + b_rkm*x[k] + b_netDepth*x[k] + b_distDown*x[k] + b_distUp*x[k] + b_distDetect*x[k] #Log Link Function
-
-    }
-  }
-
-} # end of model  
+ 
     ",fill = TRUE)
 sink()
 
 # data for the model
-win.data <- list(c_ij = c_ij, #Catch per net series
-                 x = x, #FIX THIS
-                 d = d, #length of data within each net series index and site
-                 n = n, #Net series index
-                 s = s) #Site
+win.data <- list(x = data,
+                 ncatch = nrow(data)) #Site
 
 # Initial values
-inits <- function() list(alpha_hat = rnorm(1), #Kept the norms of previous classes even though I don't know enough about the data to say if this is a really bad init choice or not
+inits <- function() list(alpha_hat = rnorm(1),
                          b_rkm = rnorm(1),
-                         b_netDepth = rlnorm(1), #double check
+                         b_netDepth = rnorm(1),
                          b_distDown = rnorm(1),
                          b_distUp = rnorm(1),
                          b_distDetect = rnorm(1),
-                         sigma = runif(1, 0, 10) #I just copied the uniform dist assumed for sigma above. Is that picking too close of an initial value?
+                         sigma = runif(1, 0, 10)
 )
 
 
@@ -71,7 +70,7 @@ nt <- 1
 nb <- 5000
 nc <- 3
 
-# run the model and store it in an object called "out"
+
 out <- jags(win.data,
             inits,
             params,
@@ -80,10 +79,6 @@ out <- jags(win.data,
             working.directory = getwd())
 
 gelman.diag(out, confidence = 0.95, transform=FALSE, autoburnin=TRUE,
-            multivariate=TRUE) #Rhat val #This is called a Brooks-Gelman Rubin Stat. It will essentially spit out a "point estimate of the potential scale reduction factor" and upper confidence limit (Here, the paper accepted values less than 1.1 as indicating convergence of chain) GOOGLE WHAT THIS IS DOING/WHAT IT MEANS, jags does this anyways
+            multivariate=TRUE) #Rhat val #This is called a Brooks-Gelman Rubin Stat
 
-# print results
-print(out, dig = 1)
 
-# ???? here, step evaluates if b is >= to 0. If model things beta is negative, will eval to 0. Probability of decline becomes 1. But calculates every at EVERY ITERATION. Will add them up so find prob pop is actually declining. Low slopes will have p < 1
-p.decline <- 1 - step(b) # probability that population is declining
